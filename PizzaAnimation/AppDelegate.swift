2a5e118e23c4e52f62583c5ac2c9218e530fcbe8
Nasser Lamei <nasserlamei21@gmail.com>
2025-02-23 16:34:06 +0200

    Added first commit in main

--- PizzaAnimation/AppDelegate.swift
+++ PizzaAnimation/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        // add first commit in main
         window = UIWindow(frame: UIScreen.main.bounds)
         let vc = StartVC(nibName: "StartVC", bundle: nil)
         window?.rootViewController = vc
